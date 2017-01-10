25c4d5720463ea9c3dabf46629cf1f44fccf02ac
ivan <melnychenko.ivan09@gmail.com>
2017-01-10 19:19:08 +0200

    from angular 2.1.1 to 2.4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import generateTSConfig = ts.generateTSConfig;
 
 describe('App', () => {
     beforeEach(() => {
