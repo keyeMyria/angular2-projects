292548275e4d0f9086a18b2eef40aea4959652e5
ivan <melnychenko.ivan09@gmail.com>
2017-02-24 00:26:33 +0200

    angular karma setting

--- config/helpers.js
+++ config/helpers.js
@@ -4,4 +4,9 @@ function root(args) {
     args = Array.prototype.slice.call(arguments, 0);
     return path.join.apply(path, [_root].concat(args));
 }
+
+var ROOTS = path.resolve(__dirname, '..');
+var root2 = path.join.bind(path, ROOTS);
+
 exports.root = root;
+exports.root2 = root2
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,11 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+describe('App test', () => {
+    it('shod be true', () => {
+        expect(true).toBe(true);
+    })
+})
