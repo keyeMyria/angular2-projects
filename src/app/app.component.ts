d30d1752962b31dcf87a65e6d9e8d80352b17414
ivan <melnychenko.ivan09@gmail.com>
2017-02-24 21:21:36 +0200

    insection sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,26 @@ export class AppComponent implements OnInit {
         appConfig.setDefaultLanguage();
     }
 
-    ngOnInit() {
+    ngOnInit () {
         this.insectionSort(this.array);
         console.log(this.array);
     }
 
-    insectionSort(arr: number[]) {
-        
+    insectionSort (arr: number[]): void {
+        for (let i = 1; i < arr.length; i++) {
+            let j = i;
+
+            while (j >= 0 && arr[j] < arr[j-1]) {
+                console.log(i)
+                let tmp = arr[j - 1];
+                arr[j-1] = arr[j];
+                arr[j] = tmp;
+                j--;
+            }
+        }
+    }
+
+    insectionSortReverse(arr: number[]): void {
+
     }
 }
