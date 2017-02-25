14a2612d8a9f02817da30b05bf77445e4fa40231
ivan <melnychenko.ivan09@gmail.com>
2017-02-25 15:49:36 +0200

    create first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,3 +3,42 @@ import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+describe('AppComponent (inline template)', () => {
+
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent] // declare test component
+        });
+
+        fixture = TestBed.createComponent(AppComponent);
+        console.log(fixture);
+        comp = fixture.componentInstance;
+
+        // query for the title <h1> by CSS element selector
+        de = fixture.debugElement.query(By.css('h1'));
+        el = de.nativeElement;
+    });
+
+    it('should display original title', () => {
+        fixture.detectChanges();
+        expect(el.textContent).toEqual(comp.title);
+    });
+
+    it('should display a different test title', () => {
+        comp.title = 'Test Title';
+        fixture.detectChanges();
+        expect(el.textContent).toContain('Test Title');
+    });
+});
+
+// describe('a', () => {
+//     it('a', () => {
+//         expect(true).toBe(true)
+//     })
+// });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,44 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 import { AppConfigService } from './app.config.service';
 
-import '../public/scss/styles.scss';
-import 'bootstrap-grid';
+// import '../public/scss/styles.scss';
+// import 'bootstrap-grid';
 
 @Component({
     selector: 'app',
     styles: [require('./app.css')],
     template: `
-    <h1 class="app">App</h1>
+    <h1 class="app">{{title}}</h1>
     `
 })
 export class AppComponent implements OnInit {
 
-    array = [23, 3, 4,  12, 53, 12, 21, 56, 41];
+    title: string = 'Test App';
 
-    constructor(private appConfig: AppConfigService) {
-        appConfig.setDefaultLanguage();
+    constructor() {
+        
     }
 
     ngOnInit () {
-        this.insectionSort(this.array);
-        console.log(this.array);
-    }
-
-    insectionSort (arr: number[]): void {
-        for (let i = 1; i < arr.length; i++) {
-            let j = i;
-
-            while (j >= 0 && arr[j] < arr[j-1]) {
-                console.log(i)
-                let tmp = arr[j - 1];
-                arr[j-1] = arr[j];
-                arr[j] = tmp;
-                j--;
-            }
-        }
-    }
-
-    insectionSortReverse(arr: number[]): void {
-
+        
     }
 }
