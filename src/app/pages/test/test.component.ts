c2e2088bee884ec4606ffad9574d180a9d8e3b1c
IvanMelnychenko <i.melnichenko@skywell.com.ua>
2017-04-07 11:31:36 +0300

    email validation function

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -3,8 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ValidationService } from '../../components/se-control-message/validation.service';
 
 @Component({
-    selector : 'test-cm',
-    template : `
+    selector: 'test-cm',
+    template: `
         <form [formGroup]="form">
             <div>
                 <input type="text" formControlName="name">
@@ -22,7 +22,8 @@ export class TestCmComponent implements OnInit {
 
     form: FormGroup;
 
-    constructor(private _fb: FormBuilder) { }
+    constructor(private _fb: FormBuilder) {
+    }
 
     ngOnInit() {
         this.form = this.formBuilder();
@@ -30,7 +31,7 @@ export class TestCmComponent implements OnInit {
 
     formBuilder() {
         return this._fb.group({
-            name: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(12)]],
+            name : ['', [Validators.required, Validators.minLength(10), Validators.maxLength(12)]],
             email: ['', [ValidationService.emailValidator]]
         });
     }
